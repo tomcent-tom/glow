7f49930165c2099bf1dcbe8ff84f4a0ed32e5026
tomcent-tom <tom.hh.evers@gmail.com>
2021-10-07 21:58:55 +1100

    Add initial Glow script

--- glow/scripts/glow.py
+++ glow/scripts/glow.py
@@ -1,6 +1,32 @@
 import click
 
-@click.command()
+@click.group()
 def cli():
-    """Example script."""
+    """Glow cli tool."""
     click.echo('Hello World!')
+
+@cli.command()
+def fetch():
+    """Fetch your definitions and store them into YML files.
+    The final result will be available in your $(definitions-path) folder as defined in the glow_project.yml file."""
+    click.echo('Compiling glow project')
+
+@cli.command()
+def compile():
+    """Compile your YAML definitions into MD files.
+    The final result will be available in your $(docs-path) folder as defined in the glow_project.yml file."""
+    click.echo('Compiling glow project')
+
+@cli.command()
+def build():
+    """Build your Glow project into a static html site.
+    The final result will be available in your $(site-path) folder as defined in the glow_project.yml file."""
+    click.echo('Building glow project')
+
+@cli.command()
+def serve():
+    """Serve your latest build via a lightweigth server. (Non-PROD only!)"""
+    click.echo('Serving glow project')
+
+if __name__ == '__main__':
+    cli()
